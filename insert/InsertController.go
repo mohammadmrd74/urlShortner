fea8573945aa1cc9979f52f7523ba82d4d147b8d
mohammad moradi <mohammadmrd74@gmail.com>
2023-07-24 17:18:46 +0330

    log

---
+++ insert/InsertController.go
@@ -0,0 +1,26 @@
+package insert
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func InsertController(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	var userRequest User
+
+	err := json.NewDecoder(request.Body).Decode(&userRequest)
+	fmt.Println(userRequest)
+	if err != nil {
+		log.Fatalln("There is an error", err)
+	}
+
+}
--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/mohammadmrd74/urlShortner/insert"
 )
 
 func main() {
-	fmt.Println("HI")
+	router := mux.NewRouter()
+	router.HandleFunc("/api/user", insert.InsertController).Methods("POST")
+
+	err := http.ListenAndServe(":8080", router)
+	fmt.Println(err)
+	if err != nil {
+		log.Fatalln("There's an error with the server,", err)
+	} else {
+		fmt.Println("Listening on port 8080")
+	}
 }
